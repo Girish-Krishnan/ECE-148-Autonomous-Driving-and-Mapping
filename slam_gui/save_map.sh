b58a8176eea7df5eb2fdeaee24a31343f4176a16
Girish Krishnan <gikrishnan@ucsd.edu>
2023-03-23 07:58:43 -0700

    trial

--- slam_gui/save_map.sh
+++ slam_gui/save_map.sh
@@ -1,3 +1,2 @@
-source_ros1
 rostopic pub syscommand std_msgs/String "savegeotiff"
 cp /home/projects/ros1_ws/src/ucsd_robocar_hub1/ucsd_robocar_nav1_pkg/maps/*.tif /home/projects/slam_gui/static/
--- slam_gui/slam_map.py
+++ slam_gui/slam_map.py
@@ -13,6 +13,7 @@ import threading
 import time
 from PIL import Image
 import glob
+import os
 
 def run_script(path):
     subprocess.check_output(["bash", path])
@@ -29,8 +30,11 @@ async def read_item(request: Request):
 @app.get("/update_map")
 async def update():
     print("Update")
-    subprocess.check_output(["bash", 'map_server.sh'])
-    subprocess.check_output(["bash", 'save_map.sh'])
+    update_script = os.path.abspath('./save_map.sh')
+    os.chdir('/home/projects/ros1_ws')
+    subprocess.call(['bash', update_script])
+
+    os.chdir('/home/projects/ECE_MAE_148_Final_Project/slam_gui')
 
     tif_file = glob.glob('./static/*.tif')[0]
 
@@ -43,25 +47,25 @@ async def update():
 
 if __name__ == "__main__":
     print('Starting server and initializing SLAM with ROS1')
-    t1 = threading.Thread(target=run_script, args=('init_terminal1.sh',))
-    t1.setDaemon(True)
-    t1.start()
-    time.sleep(5)
-    print('Starting ROS Bridge')
-    t2 = threading.Thread(target=run_script, args=('init_terminal2.sh',))
-    t2.setDaemon(True)
-    t2.start()
-    time.sleep(5)
-    print('Starting ROS2')
-    t3 = threading.Thread(target=run_script, args=('init_terminal3.sh',))
-    t3.setDaemon(True)
-    t3.start()
+    # t1 = threading.Thread(target=run_script, args=('init_terminal1.sh',))
+    # t1.setDaemon(True)
+    # t1.start()
+    # time.sleep(5)
+    # print('Starting ROS Bridge')
+    # t2 = threading.Thread(target=run_script, args=('init_terminal2.sh',))
+    # t2.setDaemon(True)
+    # t2.start()
+    # time.sleep(5)
+    # print('Starting ROS2')
+    # t3 = threading.Thread(target=run_script, args=('init_terminal3.sh',))
+    # t3.setDaemon(True)
+    # t3.start()
     
     try:
         uvicorn.run(app, host='localhost', port=8000)
     except KeyboardInterrupt:
         print('Stopping server')
         # Join threads
-        t1.join()
-        t2.join()
-        t3.join()
+        # t1.join()
+        # t2.join()
+        # t3.join()
